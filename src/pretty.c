a7f8acf94f652077c5f5b3e4538f67fe05815d12
Rajveer Gandhi <rajveergandhi.cs@gmail.com>
2018-02-27 10:55:38 -0500

    added some code that could potentially fix it. TYPE_CASTS.GO

--- src/pretty.c
+++ src/pretty.c
@@ -44,7 +44,7 @@ void prettyPrint(NODE *node) {
                         printf("\n");
                     }
                 }
-                else {
+                else if (TOPLEVEL != NULL) {
                     prettyPrint(TOPLEVEL);
                     break;
                 }
@@ -66,7 +66,7 @@ void prettyPrint(NODE *node) {
                         printf("\n");
                     }
                 }
-                else {
+                else if (TOPLEVEL != NULL) {
                     prettyPrint(TOPLEVEL);
                     break;
                 }
@@ -99,7 +99,7 @@ void prettyPrint(NODE *node) {
                     prettyPrint(node->val.toplevel.type.funcDcl.block);
                     printf("}\n");
                 }
-                else {
+                else if (TOPLEVEL != NULL) {
                     prettyPrint(TOPLEVEL);
                     break;
                 }
