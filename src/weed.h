c8265224b7ed9a7b8832f8989fce4cbab704d9af
Dipanjan Dutta <dipanjandutta92@gmail.com>
2018-03-12 17:33:50 -0400

    Added comments to weed.h

--- src/weed.h
+++ src/weed.h
@@ -1,5 +1,10 @@
 /* weeding makes a traversal of the AST and checks the following items:
- * 1. correct use of a blank identifier: The blank identifier may appear as an operand only on the left-hand side of an assignment. 
+ * 1. correct use of a blank identifier: The blank identifier may appear as an operand only on the left-hand side of an assignment or as identifier in a top-level declaration. 
+ * 2. return statements : return statements can only be inside a function body
+ * 3. break statements : break statements can only be inside a for loop, switch block or if block
+ * 4. continue statements : continue statements can only be inside a for loop
+ * 5. assignment and short declarations : The operands on L.H.S. of a short declaration or assignment must be equal to the number of expressions in the R.H.S.
+ * 6. default statement : every switch block should have exactly one default case.
  */
     
 #ifndef weed_H
@@ -7,10 +12,10 @@
 
 #include "tree.h"
 
-void weedPROGRAM(PROGRAM *node);
-void weedPACKAGE(PACKAGE *node);
-void weedTOPLEVELDECL(TOPLEVELDECL *node);
-void weedDCL(DCL *node);
+void weedPROGRAM(PROGRAM *node);						
+void weedPACKAGE(PACKAGE *node);						
+void weedTOPLEVELDECL(TOPLEVELDECL *node);				
+void weedDCL(DCL *node);								
 void weedVARDCL(VARDCL *node);
 void weedTYPEDCL(TYPEDCL *node);
 void weedFUNCDCL(FUNCDCL *node);
